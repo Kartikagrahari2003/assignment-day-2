92a6161e056b00a4670e284755e28f266a0de867
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-05-08 13:51:24 +0530

    Circle area calculator

---
+++ q4.py
@@ -0,0 +1,7 @@
+# Write a program to calculate the area of a circle
+print("Circle area calculator")
+
+r=float(input("Enter radius of circle:::"))
+area= 3.14*r*r
+
+print(f"Area of circle ::: {area}")
